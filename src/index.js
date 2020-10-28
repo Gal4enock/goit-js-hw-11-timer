8e611a85f3d3345ac339ae3ce18d666be59ae4a2
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-10-28 13:55:28 +0200

    добавляет решение

--- src/index.js
+++ src/index.js
@@ -2,18 +2,47 @@ import './styles.css';
 // Таймер обратного отсчета
 // Создай плагин настраиваемого таймера, который ведет обратный отсчет до предварительно определенной даты. Такой плагин может использоваться в блогах и интернет-магазинах, страницах регистрации событий, во время технического обслуживания и т. д.
 // Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX. Количество дней может состоять из более чем двух цифр.
-class CountdownTimer {
 
+const clockDays = document.querySelector('[data-value="days"]');
+const clockHours = document.querySelector('[data-value="hours"]');
+const clockMins = document.querySelector('[data-value="mins"]');
+const clockSecs = document.querySelector('[data-value="secs"]');
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+class CountdownTimer {
 
   constructor({selector, targetDate}) {
     this.selector = selector;
     this.targetDate = targetDate;
   }
-
-
+  start() {
+    setInterval(() => {
+    const startTime = Date.now();
+    const time = this.targetDate - startTime; 
+    this.updateTimer(time)
+    }, 1000); 
+  }
+  updateTimer(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    clockDays.textContent = days;
+    clockHours.textContent = hours;
+    clockMins.textContent = mins;
+    clockSecs.textContent = secs;
+  }
 }
+
+
 const count = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Jan 01, 2021'),
 });
-console.log(count);
+console.log(count.start());
+// Для подсчета значений используй следующие готовые формулы, где time - разница между targetDate и текущей датой.
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
