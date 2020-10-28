3ba1ef5ca20d02940061d99fdeeb85a932a15b8c
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-10-28 23:40:18 +0200

    добавляет селектор

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import './styles.css';
 // Создай плагин настраиваемого таймера, который ведет обратный отсчет до предварительно определенной даты. Такой плагин может использоваться в блогах и интернет-магазинах, страницах регистрации событий, во время технического обслуживания и т. д.
 // Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX. Количество дней может состоять из более чем двух цифр.
 
-const clockDays = document.querySelector('[data-value="days"]');
-const clockHours = document.querySelector('[data-value="hours"]');
-const clockMins = document.querySelector('[data-value="mins"]');
-const clockSecs = document.querySelector('[data-value="secs"]');
+
 function pad(value) {
   return String(value).padStart(2, '0');
 }
@@ -15,7 +12,16 @@ class CountdownTimer {
   constructor({selector, targetDate}) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.refs = this.getRefs();
   }
+  getRefs (selector) {
+  const timerBox = document.querySelector(`${this.selector}`);
+  const clockDays = timerBox.querySelector('[data-value="days"]');
+  const clockHours = timerBox.querySelector('[data-value="hours"]');
+  const clockMins = timerBox.querySelector('[data-value="mins"]');
+    const clockSecs = timerBox.querySelector('[data-value="secs"]');
+    return {clockDays, clockHours, clockMins, clockSecs}
+}
   start() {
     setInterval(() => {
     const startTime = Date.now();
@@ -23,7 +29,9 @@ class CountdownTimer {
     this.updateTimer(time)
     }, 1000); 
   }
+  
   updateTimer(time) {
+    const { clockDays, clockHours, clockMins, clockSecs } = this.refs;
     const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
@@ -34,15 +42,17 @@ class CountdownTimer {
     clockSecs.textContent = secs;
   }
 }
-
-
 const count = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jan 01, 2021'),
 });
+count.getRefs(count.selector)
 console.log(count.start());
 // Для подсчета значений используй следующие готовые формулы, где time - разница между targetDate и текущей датой.
 // const days = Math.floor(time / (1000 * 60 * 60 * 24));
 // const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 // const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 // const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+
+// console.log(getRefs(count.selector));
